4ecb26bdc087f3be4b7245de710031febd63d93a
MayraGaleanoM <maygaleano00@gmail.com>
2024-01-02 22:55:50 -0300

    boot local Storage

--- main.js
+++ main.js
@@ -4,53 +4,88 @@ const $ = (select) => document.querySelector(select);
 const $$ = (select) => document.querySelectorAll(select);
 const randomId = () => self.crypto.randomUUID();
 
-//                            Categorias Local Storage
+// Categorias Local Storage
 
-let categoryTable = [
+const bringData = () => {
+    return JSON.parse(localStorage.getItem('light'));
+};
+
+bringData('categoryTable');
+
+const bringCategory = () => {
+    return bringData()?.categoryTable;
+};
+
+let categoryTable = bringCategory() || [
     {
-        id: 1,
+        id: randomId(),
         name: 'Comida',
     },
     {
-        id: 2,
+        id: randomId(),
         name: 'Salidas',
     },
     {
-        id: 3,
+        id: randomId(),
         name: 'Educacion',
     },
     {
-        id: 4,
+        id: randomId(),
         name: 'Transporte',
     },
     {
-        id: 5,
+        id: randomId(),
         name: 'Trabajo',
     },
     {
-        id: 6,
+        id: randomId(),
         name: 'Servicio',
     },
 ];
 
+console.log(categoryTable)
 
-const renderCategory = (users) => {
-    for ( const user of users ) {
-        $('#categoryList').innerHTML += `
-        <li class="flex flex-row">
-            <span class="py-1 px-1">${user.id}</span>
-            <span  class="py-1 px-1">${user.name}</span>
-        <div class="">
-            <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-1 rounded">
-                Editar
-            </button>
-            <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-1 rounded">
-                Eliminar
-            </button>
+const renderCategory = (categorie) => {
+    for ( const {id, name} of categorie ) {
+        $('#categoryList').innerHTML += `<li class="flex flex-row  justify-between">
+        <div class='space-x-2'>
+        <p class="bg-slate-100 py-1 px-1 rounded">${name}</p>
         </div>
-        `
-        
-    }
+        <div class="">
+        <button ('${id}') class="bttnEdit bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-1 rounded">
+        Editar
+        </button>
+        <button id="${id}" class="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-1 rounded">
+        Eliminar
+        </button>
+        </div>
+        </li>`
+    };
 };
+
 renderCategory(categoryTable);
 
+// Editar Categorias
+
+const userInfo = () => {
+    return{
+        id: randomId(),
+        name: $('#inputName').value,
+    }
+}
+
+const initializeApp = () => {  
+    $('.bttnEdit').addEventListener('click', () => {
+        $('#sectionEdit').classList.remove('hidden');
+        $('#sectionCategory').classList.add('hidden');
+    })
+    
+    $("#bttnConfirm").addEventListener('click', (e) => {
+        e.preventDefault()
+        const newDate = userInfo();
+        categoryTable.push(newDate);
+        console.log(categoryTable)
+        
+    })
+};
+window.addEventListener('load', initializeApp);
