87309175f0d260db6d20c16efc7a5acdee037070
Cecilia Da Silva <ceciliaadasilva@hotmail.com>
2024-01-04 19:12:53 -0800

    nav-bar layout

---
+++ js/main.js
@@ -0,0 +1,6 @@
+/* UTILITIES */
+
+const $ = (selector) => document.querySelector(selector);
+const $$ = (selector) => document.querySelectorAll(selector);
+
+$("#hamburger-menu").classList.add("hidden")
