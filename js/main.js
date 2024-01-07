9db87f5fff8fe2c7d106cfbe7727650f177b0900
Cecilia Da Silva <ceciliaadasilva@hotmail.com>
2024-01-06 21:04:58 -0800

    hide filters, new operation js functionalities. Style correction on new operation

--- js/main.js
+++ js/main.js
@@ -4,3 +4,23 @@ const $ = (selector) => document.querySelector(selector);
 const $$ = (selector) => document.querySelectorAll(selector);
 
 $("#hamburger-menu").classList.add("hidden")
+
+// FILTER SECTION ACCORDION
+$(".hide-filters").addEventListener("click", () => {
+    $(".hide-filters").classList.toggle("hidden")
+    $(".show-filters").classList.toggle("hidden")
+    $("#filters").classList.toggle("hidden")
+})
+
+$(".show-filters").addEventListener("click", () => {
+    $(".hide-filters").classList.toggle("hidden")
+    $(".show-filters").classList.toggle("hidden")
+    $("#filters").classList.toggle("hidden")
+})
+
+// NEW OPERATION
+
+$(".new-operation-btn").addEventListener("click", () => {
+    $(".balance-container").classList.add("hidden")
+    $("#new-operation").classList.remove("hidden")
+})
