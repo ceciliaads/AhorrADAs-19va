3b0e2fbe4ba1d964427284201aea36160d1d3e83
Cecilia Da Silva <ceciliaadasilva@hotmail.com>
2024-01-07 17:14:21 -0800

    form functionalities working, new operation, add, edit buttons working as well

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-/* UTILITIES */
+//-- -------------------------- UTILITIES -------------------------- -->
 
 const $ = (selector) => document.querySelector(selector);
 const $$ = (selector) => document.querySelectorAll(selector);
@@ -21,115 +21,88 @@ const cleanContainer = (selector) => $(selector).innerHTML = ""
 const getData = (key) => JSON.parse(localStorage.getItem(key))
 const setData = (key, data) => localStorage.setItem(key, JSON.stringify(data))
 
-const defaultCategories = [
-    {
-        id: randomId(),
-        categorieName: "Food"
-    },
-    {
-        id: randomId(),
-        categorieName: "Utilties"
-    },
-    {
-        id: randomId(),
-        categorieName: "Outings"
-    },
-    {
-        id: randomId(),
-        categorieName: "Education"
-    },
-    {
-        id: randomId(),
-        categorieName: "Transportation"
-    },
-    {
-        id: randomId(),
-        categorieName: "Job"
-    }
-    ]
-
 const allOperations = getData("operations") || []
 // const allCategories = getData("categories") || defaultCategories
 
-/* RENDERS */
-const renderOperations = (operations) => {
-    cleanContainer("#operation-content")
-    if (operations.length) {
-        hideElement([".no-results"])
-        showElement([".operation-content"])
-        for (const operation of operations) {
-            const categorieSelected = getData("categories").find(categorie => categorie.id === operation.categorie)
-            $("#operation-content").innerHTML += `
-                <div flex place-content-between>
-                    <h4>${operation.description}</h4>
-                    <h4>${operation.categorie}</h4>
-                    <h4>${operation.date}</h4>
-                    <h4>${operation.amount}</h4>
-                    <h4>
-                        <button class="btn bg-green-400" onclick="showFormEdit('${operation.id}')"> Edit </button>
-                        <button type="button" class="btn bg-red-400"> Delete</button>
-                    </h4>
-                </tr>
-            `
-        }
-    } else {
-        // showElement([".no-results"])
-        hideElement([".operation-content"])
-    }
-}
+const operationsPlaceHolder = []
 
-const saveOperation = (operation) => {
-    return {
-        id: 3,
-        description: $("#description-input").value,
-        categorie: $("#ategories-select").value,
-        // date: $("#date-input").value,
-        amount: $("#amount-input").value,
-        balanceType: $("#type-select-operation").value
+//-- -------------------------- RENDERS -------------------------- -->
+
+const renderOperations = (operations) => {
+    for (const operation of operations) {
+        $("#table").innerHTML += `
+        <tr> 
+            <td>${operation.description}</td>
+            <td>${operation.categorie}</td>
+            <td>${operation.date}</td>
+            <td>${operation.amount}</td>
+            <td>
+                <button class="btn text-blue-400" onclick="showFormEdit('${operation.id}')"> Edit </button>
+                <button type="button" class="btn text-blue-400"> Delete</button>
+            </td>
+        </tr>
+        
+        `
     }
 }
+console.log(operationsPlaceHolder)
+
+// renderOperations(operationsPlaceHolder)
+
+const saveNewOperation = () => {
+    return {
+        id: randomId(),
+        description: $("#description-input").value,
+        categorie: $("#categories-select").value,
+        date: $("#date-input").value,
+        amount: $("#amount-input").value,
+    }
+}
+console.log(saveNewOperation())
 
 const showFormEdit = (operationId) => {
-    hideElement([".table","#add-operation-btn",".label-description"])
-    showElement([".form", "#edit-operation-btn",".label-edit"])
+    showElement(["#new-operation", "#edit-operation-btn", "#edit-operation-title"])
+    hideElement([".table-operations", ".balance-container", "#add-operation-btn", "#new-operation-title"])
     $("#edit-operation-btn").setAttribute("data-id", operationId)
     const operationSelected = getData("operations").find(operation => operation.id === operationId)
     $("#description-input").value = operationSelected.description
-    $("#amount-input").value = operationSelected.amount
-    $("#type-select-operation").value = operationSelected.balanceType
     $("#categories-select").value = operationSelected.categorie
     $("#date-input").value = operationSelected.date
+    $("#amount-input").value = operationSelected.amount
+    // add type of balance
+
+    
 }
-// EVENTS
+
+
+//-- -------------------------- EVENTS -------------------------- -->
+
 const initializeApp = () => {
     setData("operations", allOperations)
     renderOperations(allOperations)
 
-    $(".hide-filters").addEventListener("click", () => {
-        $(".hide-filters").classList.toggle("hidden")
-        $(".show-filters").classList.toggle("hidden")
-        $("#filters").classList.toggle("hidden")
-    })
-    
-    $(".show-filters").addEventListener("click", () => {
-        $(".hide-filters").classList.toggle("hidden")
-        $(".show-filters").classList.toggle("hidden")
-        $("#filters").classList.toggle("hidden")
+    $("#new-operation-btn").addEventListener("click", () => {
+        $("#new-operation").classList.remove("hidden")
+        $(".balance-container").classList.add("hidden")
     })
 
     $("#add-operation-btn").addEventListener("click", (e) => {
         e.preventDefault()
         const currentData = getData("operations")
-        currentData.push(saveOperation())
+        currentData.push(saveNewOperation())
         setData("operations", currentData)
+        $(".no-results-container").classList.add("hidden")
+        $(".balance-container").classList.remove("hidden")
+        $(".table-operations").classList.remove("hidden")
+        $("#new-operation").classList.add("hidden")
     })
 
     $("#edit-operation-btn").addEventListener("click", (e) => {
-        e.preventDefault(e)
+        e.preventDefault()
         const operationId = $("#edit-operation-btn").getAttribute("data-id")
         const currentData = getData("operations").map(operation => {
-            if(operation.id === operationId) {
-                return saveOperation()
+            if(operation.id == operationId) {
+                return saveNewOperation()
             }
             return operation
         })
@@ -144,43 +117,42 @@ window.addEventListener("load", initializeApp)
 
 
 
+// // MIO
+// $("#hamburger-menu").classList.add("hidden")
 
-// MIO
-$("#hamburger-menu").classList.add("hidden")
+// // FILTER SECTION ACCORDION
+// // $(".hide-filters").addEventListener("click", () => {
+// //     $(".hide-filters").classList.toggle("hidden")
+// //     $(".show-filters").classList.toggle("hidden")
+// //     $("#filters").classList.toggle("hidden")
+// // })
 
-// FILTER SECTION ACCORDION
-// $(".hide-filters").addEventListener("click", () => {
-//     $(".hide-filters").classList.toggle("hidden")
-//     $(".show-filters").classList.toggle("hidden")
-//     $("#filters").classList.toggle("hidden")
+// // $(".show-filters").addEventListener("click", () => {
+// //     $(".hide-filters").classList.toggle("hidden")
+// //     $(".show-filters").classList.toggle("hidden")
+// //     $("#filters").classList.toggle("hidden")
+// // })
+
+// // NEW OPERATION
+
+// $(".new-operation-btn").addEventListener("click", () => {
+//     $(".balance-container").classList.add("hidden")
+//     $("#new-operation").classList.remove("hidden")
 // })
 
-// $(".show-filters").addEventListener("click", () => {
-//     $(".hide-filters").classList.toggle("hidden")
-//     $(".show-filters").classList.toggle("hidden")
-//     $("#filters").classList.toggle("hidden")
+// // CANCEL BUTTON NEW OPERATION
+
+// $("#cancel-operation-btn").addEventListener("click", (e) => {
+//     e.preventDefault()
+//     $(".balance-container").classList.remove("hidden")
+//     $("#new-operation").classList.add("hidden")
 // })
 
-// NEW OPERATION
+// // BALANCE NAV BAR
 
-$(".new-operation-btn").addEventListener("click", () => {
-    $(".balance-container").classList.add("hidden")
-    $("#new-operation").classList.remove("hidden")
-})
-
-// CANCEL BUTTON NEW OPERATION
-
-$("#cancel-operation-btn").addEventListener("click", (e) => {
-    e.preventDefault()
-    $(".balance-container").classList.remove("hidden")
-    $("#new-operation").classList.add("hidden")
-})
-
-// BALANCE NAV BAR
-
-$("#balance-nav").addEventListener("click", () => {
-    $("#new-operation").classList.add("hidden")
-    // $("#categories-section").classList.add("hidden")
-    // $("#report-section").classList.add("hidden")
-    $(".balance-container").classList.remove("hidden")
-})
+// $("#balance-nav").addEventListener("click", () => {
+//     $("#new-operation").classList.add("hidden")
+//     // $("#categories-section").classList.add("hidden")
+//     // $("#report-section").classList.add("hidden")
+//     $(".balance-container").classList.remove("hidden")
+// })
